18e1767f0bad05e1c70e65e0d859ba731b98eb12
informatica <informaticadipafront@gmail.com>
2023-09-08 12:56:43 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
-  title = 'dipapubli';
+  title = 'DIPAPUBLI';
+  pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
 }
